d8602291e3afcc4588bd7ca79b799e9c1d1735d9
Elucidation <elucidation@gmail.com>
2011-07-31 15:54:07 -0400

    Base stats

--- Body.py
+++ Body.py
@@ -1,2 +1,14 @@
 class Body:
     """Defines a body, containing mass, 2d position & velocity"""
+    def __init__(self):
+        Body(0,0,0,0,0)
+
+    def __init__(self,x=0,y=0,vx=0,vy=0,m=1):
+        self.x = x;
+        self.y = y;
+        self.vx = vx;
+        self.vy = vy;
+        self.m = m;
+
+    def __str__(self):
+        return "%d %d %d %d %d" % (self.x,self.y,self.vx,self.vy,self.m)
---
+++ main.py
@@ -0,0 +1,5 @@
+from Body import *
+
+a = Body()
+
+print a
